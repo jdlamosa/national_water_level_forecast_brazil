e6a2b4a60bd9746cb6cd040a131c1f7e638e9f1d
Jéssica Domingues Lamosa <jdlamosa@gmail.com>
2023-05-10 00:50:34 -0300

    Create ottobaciasIndex.js

---
+++ tethysapp/national_water_level_forecast_brazil/public/js/ottobaciasIndex.js
@@ -0,0 +1,18 @@
+let ottobacias_index = {
+	  'level_1': {
+			'name': 'Ottobacia L1',
+			'geojsons': ['ottobacias_nivel1.json']
+	  },
+	  'level_2': {
+			'name': 'Ottobacia L2',
+			'geojsons': ['ottobacias_nivel2.json']
+	  },
+	  'level_3': {
+			'name': 'Ottobacia L3',
+			'geojsons': ['ottobacias_nivel3.json']
+	  },
+	  'level_4': {
+			'name': 'Ottobacia L4',
+			'geojsons': ['ottobacias_nivel4.json']
+	  },
+};
